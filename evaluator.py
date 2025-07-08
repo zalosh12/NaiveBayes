f4b042f8afdddf1a52d046fbaadeeb774f66ca4d
Elazar <e0548448478@gmail.com>
2025-07-08 23:13:26 +0300

    the first failure

---
+++ evaluator.py
@@ -0,0 +1,18 @@
+class Evaluator:
+    def __init__(self,classifier):
+        self.classifier = classifier
+
+    def evaluate_data_frame(self,df,class_column):
+        correct = 0
+        total = len(df)
+
+        for _,row in df.iterrows():
+            old_res = row[class_column]
+            row = row.drop(class_column)
+            if self.predict_single(row) == old_res:
+                correct += 1
+        correct_percent = correct / total * 100
+        return correct_percent
+
+    def predict_single(self, sample_record) :
+        return self.classifier.predict(sample_record)
---
+++ naive_bayes_classifier.py
@@ -0,0 +1,49 @@
+class NaiveBayesClassifier:
+    def __init__(self):
+       self.model = {}
+       self.class_priors = {}
+       self.features = []
+
+    def create_model(self,df,class_column):
+
+        df = df.set_index('Index')
+
+        self.class_priors = df[class_column].value_counts(normalize=True).to_dict()
+        self.features = [i for i in df.columns if i != class_column]
+        lables = df[class_column].unique()
+        print(features,lables)
+
+        for lable in lables:
+            self.model[lable] = {}
+
+            sub_df = df[df[class_column] == lable]
+
+            for feature in self.features:
+                value_count = sub_df[feature].value_counts()
+                total =len(sub_df)
+
+                possible_values = df[feature].unique()
+                probs = {}
+
+                for val in possible_values:
+                    count = value_count.get(val,0)
+                    probs[val] = (count + 1) / (total + len(possible_values))
+
+                self.model[lable][feature] = probs
+
+    def predict(self,row):
+        res = {}
+        for label in self.model.keys():
+            prob = self.class_priors[label]
+            for fearure in self.model[label].keys():
+                val_input = row[fearure]
+                prob *= self.model[label][fearure].get(val_input,1e-6)
+            res[label] = prob
+        return max(res,key=res.get)
+
+
+
+
+
+
+
